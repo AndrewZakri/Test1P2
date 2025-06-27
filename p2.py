0cba12874c1969a73c4d8c9a3f1ce108314b081b
AndrewZakri <zakria@sunypoly.edu>
2025-06-27 12:53:03 -0400

    Update p2.py

--- p2.py
+++ p2.py
@@ -4,8 +4,8 @@ import matplotlib.pyplot as plt
 import seaborn as sns
 
 # Label Dashboard and configure layout
-st.set_page_config(page_title="University Student Dashboard", layout="wide")
-st.title("University Student Dashboard")
+st.title("Test 2, Problem 2")
+st.subheader("University Student Dashboard")
 
 # Load the provided CSV for problem 2
 df = pd.read_csv("university_student_dashboard_data.csv")
